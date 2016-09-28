0b642bdd8abf6c37caf7019799f145e4ea329e41
Anshul Sahni <anshul_sahni@live.com>
2016-09-28 13:00:06 +0530

    added comments in file containing config variables

--- config.js
+++ config.js
@@ -1,6 +1,26 @@
 module.exports = {
+
+  /**
+   * Root directory of the js source code
+   */
   ROOT_DIR: '',
+
+  /**
+   * Root directory for the app
+   * @type {String}
+   */
   APP_ROOT_DIR: 'app',
+
+  /**
+   * name of js file to be loaded in the final project
+   * @type {String}
+   */
   MAIN_JS_FILE: 'main.js',
+
+  /**
+   * extensions to be used by browserify or watchify
+   * @type {Array}
+   */
   BROWSERIFY_EXTENSIONS: ['.jsx', '.js']
+
 };
