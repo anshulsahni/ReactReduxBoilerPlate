676c169a59dd74c1f5fb3a366806ed55583b033f
Anshul Sahni <anshul_sahni@live.com>
2016-10-03 17:29:02 +0530

    fixed bug - specified the extensions at the right place in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -48,13 +48,15 @@ gulp.task('lint', function() {
  * task to bundle all the javscript files from scripts folder
  */
 gulp.task('scripts', function() {
-  var bundler = watchify(browserify(config.ROOT_DIR + '/index.jsx'), {
-    extensions: config.BROWSERIFY_EXTENSIONS
-  });
+  var bundler = watchify(browserify(config.ROOT_DIR + '/index.jsx', {
+    extensions: config.BUNDLING_EXTENSIONS
+  }));
 
   var compile = function() {
     bundler
-      .transform('babelify')
+      .transform('babelify', {
+        extensions: config.BUNDLING_EXTENSIONS
+      })
       .bundle()
       .pipe(source(config.MAIN_JS_FILE))
       .pipe(gulp.dest(config.APP_ROOT_DIR + '/build'));
